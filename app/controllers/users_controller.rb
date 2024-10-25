7705c8d65f2037a3474817df161f1e9e16b5a9df
mradul <mradul.codenote@gmail.com>
2024-10-25 13:11:20 +0530

    display leaderboard at hom e page also

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 	class UsersController < ApplicationController
-	def index
+	def index  
+   @rank = User.order(:rank)
 	end
   
   def show
@@ -9,7 +10,7 @@
   end
   
 	def new
-		@user = User.new
+	 @user = User.new
 	end
 
 	def create
